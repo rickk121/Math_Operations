c986d151588854352096680afb25a100ef1f146f
Ricky Parnell <Rickyp2019@gmail.com>
2024-11-17 21:21:12 -0500

    added n ew fucntion that displays the sum of the two numbers given

--- math_ops.py
+++ math_ops.py
@@ -6,9 +6,17 @@ def divide_numbers(a, b):
         print("Cannot divide by 0")
     else:
         return a / b  # Potential division by zero error
+    
+def add_numbers(a, b):
+    """Adds two numbers and returns the result."""
+    return a + b
 
 if __name__ == "__main__":
     x = 10
     y = 0
     result = divide_numbers(x, y)
+    
     print(f"The result of division is: {result}")
+
+    result = add_numbers(x, y)
+    print(f"The result of addition is: {result}")
